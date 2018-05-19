17cbc00d99b2e6f580e0667794fac24c558e9342
Anderson Tsang <andyderson@gmail.com>
2018-05-19 11:15:38 -0700

    Initial Setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-class App extends React.Cfomponent {
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
